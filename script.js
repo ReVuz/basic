cfc2fb94fbf1eecbb2c2a85279c6302a6d890e16
Revathy <revathymr916@gmail.com>
2024-04-27 20:42:23 +0530

    Pictures and code updated

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// Add event listeners to all cards
-const cards = document.querySelectorAll('.card');
-cards.forEach(card => {
-    card.addEventListener('mouseover', () => {
-        const img = card.querySelector('img');
-        img.style.transform = 'scale(1.1)';
-    });
-
-    card.addEventListener('mouseout', () => {
-        const img = card.querySelector('img');
-        img.style.transform = 'scale(1)';
-    });
+$(document).ready(function() {
+$(".card").each(function() {
+var imageSrc = $(this).attr("data-image-src");
+$(this).append(`<img src="${imageSrc}">`);
+});
 });
